1450bbe2f6f6c53a65a9e8639a2359c602712ec6
Mike <70848378+asterus21@users.noreply.github.com>
2022-01-08 20:01:38 +0300

    Add files via upload

---
+++ golangCode/palyndromeOne.go
@@ -0,0 +1,40 @@
+// package main
+
+package palyndrome
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func main() {
+
+	var wordOne = "tenet"
+
+	wordToCheck := toSplit(wordOne)
+	isPalyndrome(wordToCheck)
+	// splitted := strings.Split(wordOne, "")
+	// fmt.Println(splitted)
+	// fmt.Println(reflect.TypeOf(splitted))
+}
+
+func toSplit(word string) []string {
+	wordToSplit := strings.Split(word, "")
+	// fmt.Println(wordToSplit[0])
+	fmt.Println(wordToSplit)
+	return wordToSplit
+
+}
+
+func isPalyndrome(wordToCheck []string) bool {
+	for i := 0; i < len(wordToCheck)+1; i++ {
+		if reflect.DeepEqual(wordToCheck[i:], wordToCheck[:1]) {
+			// fmt.Println(wordToCheck)
+			fmt.Println("true")
+			return true
+		}
+	}
+	fmt.Println("false")
+	return false
+}
